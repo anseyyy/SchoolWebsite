95133e77378ef70865ee36583bcf93b7d01ef7be
anseyyy <ahamedansil43@gmail.com>
2025-11-21 00:08:40 +0530

    backend updated

--- controller/userController.js
+++ controller/userController.js
@@ -1,45 +1,42 @@
-const UserModel = require("../models/userModel")
-const bcrypt = require("bcrypt")
+const UserModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 exports.UserData = async (req, res) => {
   console.log("Inside UserData ");
+  const { name, email, password } = req.body
 
   try {
-    const { name, email,  password } = req.body;
+    const existingUser = await Users.findOne({ email })
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ message: "User already exists. Please log in." })
+    }
 
-    const newUser = await UserModel.create({
-      name,
-      email,
-      password
-    })
+    const newUser = new User({ email, password, name })
+    await newUser.save()
 
-    res.status(201).json({ success: true, message: "User registered successfully", userType: newUser.userType });
+    res.status(201).json({ message: "User registered successfully." })
   } catch (err) {
-    console.error("Error creating user:", err.message);
-    res.status(500).json({ error: "Failed to create user" });
+    res.status(500).json({ message: "Server error", err })
   }
-
-
-}
-
+};
 
 exports.loginUser = async (req, res) => {
-  console.log("Inside loginUser")
+  console.log("Inside loginUser");
 
   const { email, password } = req.body;
-  console.log({email,password});
-  
-  const adminEmail = "admin@gmail.com"
-  const adminPassword = "admin123"
-try{
-  if (email === adminEmail && password === adminPassword) {
-     res.status(200).json({ success: true })
-  } else {
-     res.status(406).json({ error: "Invalid credentials" })
-  }
-}
-catch(err){
-  res.status(400).json(err)
+  console.log({ email, password });
 
-}
-}
+  const adminEmail = "admin@gmail.com"
+  const adminPassword = "admin123";
+  try {
+    if (email === adminEmail && password === adminPassword) {
+      res.status(200).json({ success: true });
+    } else {
+      res.status(406).json({ error: "Invalid credentials" });
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
