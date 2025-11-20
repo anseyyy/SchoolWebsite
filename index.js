0a91bdd5567867831118c55e057c58150070e59a
anseyyy <ahamedansil43@gmail.com>
2025-11-20 18:18:09 +0530

    Cors

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require ('express')
-const  mongoose  = require('mongoose')
-const cors = require('cors')
-require('dotenv').config();
-require ('./dbConnection/dbConnection')
-const router=require('./routes/routes')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+require("./dbConnection/dbConnection");
+const router = require("./routes/routes");
 
+const backend = express();
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
 
+backend.use(express.json());
+backend.use(cors(corsOptions));
+backend.use(router);
 
-const backend = express()
-backend.use(express.json())
-backend.use(cors())
-backend.use(router)
+const PORT = process.env.PORT || 3000;
 
-
-const PORT = process.env.PORT || 3000   
-
-backend.listen(PORT,()=>{
-    console.log(`Server is Running ${PORT}`);
-    
-})
-
+backend.listen(PORT, () => {
+  console.log(`Server is Running ${PORT}`);
+});
